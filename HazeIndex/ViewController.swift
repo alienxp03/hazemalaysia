1dd38685d23be98c7058a6563189f71a946a8cac
Azuan <alienxp03@gmail.com>
2015-10-13 13:07:05 +0800

    Initial Commit

---
+++ HazeIndex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HazeIndex
+//
+//  Created by Muhammad Azuan Zira Zairein on 13/10/2015.
+//  Copyright © 2015 Muhammad Azuan Zira Zairein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
